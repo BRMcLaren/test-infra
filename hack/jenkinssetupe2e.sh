2638db2b8e6ae947ea222926f14118a34836a0e9
Brett McLaren <brmclare@microsoft.com>
2020-08-20 12:41:03 +0000

    checkpoint for jenkins integration
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- hack/jenkinssetupe2e.sh
+++ hack/jenkinssetupe2e.sh
@@ -1,12 +1,11 @@
-# Experimental
 az group create \
     --output table \
-    --name OE-Prow-Jenkins-Testing \
+    --name OE-Prow-Testing \
     --location uksouth
 
 az vm create \
     --output table \
-    --resource-group OE-Prow-Jenkins-Testing \
+    --resource-group OE-Prow-Testing \
     --location uksouth \
     --name jenkins-master-testing \
     --size Standard_DS3_v2 \
@@ -18,7 +17,7 @@ az vm create \
     --admin-username oeadmin \
     --ssh-key-value ~/.ssh/id_rsa.pub
 
-NIC_ID=`az vm show --resource-group OE-Prow-Jenkins-Testing \
+NIC_ID=`az vm show --resource-group OE-Prow-Testing \
                     --name jenkins-master-testing \
                     --output tsv \
                     --query "networkProfile.networkInterfaces[0].id"`
@@ -31,7 +30,7 @@ NSG_NAME=`az network nsg show --ids $NSG_ID \
                                 --output tsv \
                                 --query "name"`
 
-az network nsg rule create --resource-group OE-Prow-Jenkins-Testing \
+az network nsg rule create --resource-group OE-Prow-Testing \
                             --nsg-name $NSG_NAME \
                             --name Port_443 \
                             --priority 500 \
@@ -40,7 +39,7 @@ az network nsg rule create --resource-group OE-Prow-Jenkins-Testing \
                             --protocol Tcp \
                             --destination-port-ranges 443
 
-az network nsg rule create --resource-group OE-Prow-Jenkins-Testing \
+az network nsg rule create --resource-group OE-Prow-Testing \
                             --nsg-name $NSG_NAME \
                             --name Port_80 \
                             --priority 501 \
@@ -48,16 +47,3 @@ az network nsg rule create --resource-group OE-Prow-Jenkins-Testing \
                             --direction Inbound \
                             --protocol Tcp \
                             --destination-port-ranges 80
-
-NIC_ID=`az vm show --resource-group OE-Prow-Jenkins-Testing \
-                    --name jenkins-master-testing \
-                    --output tsv \
-                    --query "networkProfile.networkInterfaces[0].id"`
-
-PUBLIC_IP_ID=`az network nic show --ids $NIC_ID \
-                                    --output tsv \
-                                    --query "ipConfigurations[0].publicIpAddress.id"`
-
-az network public-ip update --ids $PUBLIC_IP_ID \
-                            --dns-name oe-prow-jenkins-testing \
-                            --idle-timeout 30
